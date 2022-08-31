f1098a78fa1f5de527e547b3d0942a2f10c4532d
Aliyousafzai <warisallah923@gmail.com>
2022-08-31 13:23:07 +0500

    fetch from mongo

--- 26_node+mongo.js
+++ 26_node+mongo.js
@@ -1,16 +1,14 @@
-// import mongoClient from mongoDb package
+// Import mongoclient from mongo db p[ackage
 const {MongoClient} = require('mongodb');
 
 const url = "mongodb://localhost:27017";
-let database = "ecommerce";
-
-// passing url to the mongo client and result will be stored in client
-const client = new MongoClient(url);
+const database = "ecommerce";
+const client  = new MongoClient(url);
 
 async function getData(){
     let result = await client.connect();
     let db = result.db(database);
-    let collection = db.collection('products');
+    let collection = db.collection("products");
     let response = await collection.find({}).toArray();
     console.log(response);
 }
